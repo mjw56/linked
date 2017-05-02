638f10d2c77b047d0f7856a5cb2c1d1db392b691
Mike Wilcox <mwilcox56@gmail.com>
2017-05-02 17:58:30 -0400

    cleanup, add arrayToList fn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function makeList() {
 
   for (let i = 1; i < 10; i++) {
   	let node = new Node(i);
-	head = insert(node, head);
+	  head = insert(node, head);
   }
 
   return head;
@@ -24,7 +24,7 @@ function insert(node, head) {
   // empty list just insert at the beginning
   if (head === null) {
   	head = node;
-	return head;
+	  return head;
   }
 
   let curr = head;
@@ -52,8 +52,8 @@ function remove(value, head) {
     // set the prev to where we are right now
     prev = curr;
 
-	// set the curr to the next one
-	curr = curr.next;
+    // set the curr to the next one
+    curr = curr.next;
   }
 }
 
@@ -64,16 +64,49 @@ function reverse(head) {
 
   while (curr && curr.next !== null) {
     let temp = Object.assign({}, curr);
- 	temp.next = reversed;
-	reversed = temp;
-	curr = curr.next;
+ 	  temp.next = reversed;
+	  reversed = temp;
+	  curr = curr.next;
   }
 
   return reversed;
 }
 
+function printList(head) {
+  let curr = head;
+
+  while (curr !== null) {
+    console.log(curr.data);
+    curr = curr.next;
+  }
+}
+
+// convert an array to a linked list
+function arrayToList(array) {
+  if (array === null || array.length === 0) {
+    return null;
+  }
+
+  let head = new Node(array[0]);
+
+  if (array[1] === null) {
+    return head;
+  }
+
+  let curr = head;
+
+  for (var i = 1; i < array.length; i++) {
+    let node = new Node(array[i]);
+    curr.next = node;
+    curr = node;
+  }
+
+  return head;
+}
+
 module.exports = {
 	makeList: makeList,
+  arrayToList: arrayToList,
 	insert: insert,
 	remove: remove,
 	reverse: reverse
